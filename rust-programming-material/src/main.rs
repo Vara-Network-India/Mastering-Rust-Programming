c6596f0b0d33bed633ec54eb452609f7e9e8d203
vishal-sys <vishalsaroj6363@.com>
2024-06-24 06:11:51 +0000

    Changes

--- rust-programming-material/src/main.rs
+++ rust-programming-material/src/main.rs
@@ -1,5 +1,4 @@
-fn main() {
-    println!("https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e");
+"https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e
 
-     
-}
+Note: Use https://freedium.cfd/ to overcome from subscriptions {Copy the above link and paste to the freedium website}
+Happy learning "
