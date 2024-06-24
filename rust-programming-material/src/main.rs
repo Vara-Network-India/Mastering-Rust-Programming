5a889d4d54687401533d38b13afbe9bbea225773
vishal-sys <vishalsaroj6363@.com>
2024-06-24 08:07:51 +0000

    Submit

--- rust-programming-material/src/main.rs
+++ rust-programming-material/src/main.rs
@@ -1,12 +1,12 @@
-"https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e"
+"https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e" for (A-Z rust with projects)
 
-"https://web3.career/learn-web3/top-rust-open-source-projects"
+"https://web3.career/learn-web3/top-rust-open-source-projects" for (Open source project)
 
-"https://eleftheriabatsou.medium.com/"
+"https://eleftheriabatsou.medium.com/"  for (Begginers projects)
 
 
 
-"https://medium.com/rustaceans"
+"https://medium.com/rustaceans"  (Rust Community)
 
 Note: Use "https://freedium.cfd/"  to overcome from subscriptions {Copy the above link and paste to the freedium website}
 Happy learning "
