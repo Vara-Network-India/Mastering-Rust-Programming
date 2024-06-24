b1ad4cb527f5347cc16d219436cee72c1f2a6524
vishal-sys <vishalsaroj6363@.com>
2024-06-24 06:09:02 +0000

    Changes

--- rust-programming-material/src/main.rs
+++ rust-programming-material/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("This is the link of Rust Mastery where u will learn rust from basics to advance.
-     https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e");
+    println!("https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e");
+
+     
 }
