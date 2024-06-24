a7bb63446ea8c7d9909abfe53664a612773297f5
vishal-sys <vishalsaroj6363@.com>
2024-06-24 06:15:50 +0000

    Submit

--- rust-programming-material/src/main.rs
+++ rust-programming-material/src/main.rs
@@ -1,4 +1,6 @@
-"https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e
+"https://medium.com/@luishrsoares/list/mastering-rust-programming-06763d788b6e"
+
+"https://medium.com/rustaceans"
 
 Note: Use https://freedium.cfd/ to overcome from subscriptions {Copy the above link and paste to the freedium website}
 Happy learning "
